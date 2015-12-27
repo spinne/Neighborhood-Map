8dcef8a7250fa71f4d8876f55b00ec2f22f631e2
Sylvia Schmidt <sylvia.schmidt@jielo.de>
2015-12-27 13:36:09 +0100

    working markers

--- dev/js/app.js
+++ dev/js/app.js
@@ -3,94 +3,112 @@
 /*### Model ###*/
 
 // Array with 5 basic locations and infos
-var myNeighborhood = [
-	{
-		name: 'Kriminalmuseum, Rothenburg ob der Tauber',
-		description: 'If medival torture is for you, this is the place to visit.',
-		category: 'Museum, Indoor',
-		location: '(49.3755982, 10.179146899999978)'
-	},
-	{
-		name: 'Molkerei, Rothenburg ob der Tauber',
-		description: 'A nice bar and restaurant with occational live music.',
-		category: 'Restaurant, Bar, Food, Music, Indoor',
-		location: '(49.3808427, 10.188578300000017)'
-	},
-	{
-		name: 'Reiterlesmarkt, Rothenburg ob der Tauber',
-		description: 'Food, drink and Christmas: The famous Christmas market held every december.',
-		category: 'Event, Food, Outdoor',
-		location: '(49.3768724, 10.17927510000004)'
-	},
-	{
-		name: 'Lotus-Garten, Rothenburg ob der Tauber',
-		description: 'A tranquil south-east asian garden with café in the middle of Europe.',
-		category: 'Restaurant, Food, Outdoor',
-		location: '(49.3677609, 10.200424399999974)'
-	},
-	{
-		name:'Rödertor, Rothenburg ob der Tauber',
-		description: 'One of the gates through the intact medival city wall.',
-		category: 'Outdoor, Landmark',
-		location: '(49.37656759999999, 10.184596300000067)'
-	}
-];
-
-// Managing all data and storing it in local storage
-var manageData = function() {
-	var self = this;
+var manageData = {
+	myNeighborhood: [
+		{
+			name: 'Kriminalmuseum',
+			description: 'If medival torture is for you, this is the place to visit.',
+			category: 'Museum, Indoor',
+			lat: '49.3755982',
+			lng: '10.179146899999978'
+		},
+		{
+			name: 'Molkerei',
+			description: 'A nice bar and restaurant with occational live music.',
+			category: 'Restaurant, Bar, Food, Music, Indoor',
+			lat: '49.3808427', 
+			lng: '10.188578300000017'
+		},
+		{
+			name: 'Reiterlesmarkt',
+			description: 'Food, drink and Christmas: The famous Christmas market held every december.',
+			category: 'Event, Food, Outdoor',
+			lat: '49.3768724',
+			lng: '10.17927510000004'
+		},
+		{
+			name: 'Lotus-Garten',
+			description: 'A tranquil south-east asian garden with café in the middle of Europe.',
+			category: 'Restaurant, Food, Outdoor',
+			lat: '49.3677609',
+			lng: '10.200424399999974'
+		},
+		{
+			name:'Burggarten',
+			description: 'The place where the castle used to be is now the city park with beautiful views of the old city.',
+			category: 'Park, Outdoor, Landmark',
+			lat: '49.3762149', 
+			lng: '10.1740536'
+		}
+	],
 	
-	self.addMarker = function(map, placeData) {
+	mapMarker: [],
+	
+	init: function() {
+		
+	},
+	
+	createMarker: function() {
+		this.myNeighborhood.forEach(function(location) {
+			var marker = new Marker(map, location);
+			manageData.mapMarker.push(marker);
+		});
+		
+		vm.markerList = manageData.mapMarker;
 	}
 }
 
+
 // Class for map markers
-var Marker = function(map, name, address, location) {
+var Marker = function(map, currentData) {
 	var marker;
+	var name = currentData.name;
+	var dataLat = currentData.lat;
+	var dataLng = currentData.lng;
 	
-	this.name = ko.observable(name);
-	this.position  = ko.observable(location);
-	//this.address = ko.observable(address);
+	var location = new google.maps.LatLng(dataLat,dataLng);
 	
 	marker = new google.maps.Marker({
 			map: map,
 			position: location,
-			title: name,
-			//address: address
+			title: name
 	});
 	
-	console.log('n: ' + name + 'l: ' + location);
-	//marker.setVisible(false);
-/*
-  this.isVisible = ko.observable(false);
-
-  this.isVisible.subscribe(function(currentState) {
-    if (currentState) {
-      marker.setMap(map);
-    } else {
-      marker.setMap(null);
-    }
-  });
-
-  this.isVisible(true);
-  */
+	marker.infowin = new google.maps.InfoWindow({
+		content: 'Loading ...'
+	});
+	
+	marker.handleClick = function() {
+		marker.infowin.open(map, marker);
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout(function () {
+			marker.setAnimation(null);
+			}, 1400);
+	}
+	
+	marker.addListener('click', function() {
+		marker.handleClick();
+	});
+	
+	return marker;
 }
 
-/*### ViewModels ###*/	
+// Google Maps
+
+var map;
 
 var initMap = function() {
 	var self = this;
 	
 	// Fill the map object and select the map div for display.
-	var map = new google.maps.Map(document.getElementById('map'));
+	map = new google.maps.Map(document.getElementById('map'));
 	
-	// Make the map fit the height of the screen
+	// Make the map fit the height of the screen with 1% margin
 	self.scaleMap = function() {
 		//Calculate and set the height of the map element
 		var mapHeight = ($(window).height() - $('header').outerHeight(true))*0.98;
 		$('#map').height(mapHeight);
 	}
-	scaleMap();
 	
 	// Adjust map on resize
 	self.resizeMap = function(){
@@ -98,53 +116,24 @@ var initMap = function() {
 		map.fitBounds(mapBounds);
 	}
 	
-	// Find the locations on the map
-	self.createMarker = function(placeData) {
-		var lat = placeData.geometry.location.lat();
-		var lon = placeData.geometry.location.lng();
-		var address = placeData.formatted_address;
-		var name = placeData.name;
-		var location = placeData.geometry.location;
-		var bounds = window.mapBounds;
-		
-		/* Create a marker and set its position.
-		var marker = new google.maps.Marker({
-			map: map,
-			position: placeData.geometry.location,
-			title: name,
-			address: address
-		});*/
-		
-		//var marker = new Marker(map, name, address, location);
-		
-		// Fit the map to the markers
-		bounds.extend(new google.maps.LatLng(lat, lon));
-		map.fitBounds(bounds);
-		map.setCenter(bounds.getCenter());
-	}
-	
-	// Find the location in myNeighborhood with Google places
+	// Center map around locations in myNeighborhood
 	self.findLocation = function() {
-		var service = new google.maps.places.PlacesService(map);
-
-		for (var place in myNeighborhood) {
-			var request = {
-					query: myNeighborhood[place].name
-				};
-			
-			service.textSearch(request, callback);
-		}
+		var bounds = window.mapBounds;
+		manageData.myNeighborhood.forEach(function(place){
+			bounds.extend(new google.maps.LatLng(place.lat, place.lng));
+			map.fitBounds(bounds);
+			map.setCenter(bounds.getCenter());
+		});
 	}
 	
-	// Callback for Google Places
-	self.callback = function(results, status) {
-		if (status == google.maps.places.PlacesServiceStatus.OK) {
-			createMarker(results[0]);
-		}
-	}
-	
+	scaleMap();
 	window.mapBounds = new google.maps.LatLngBounds();
 	findLocation();
+	manageData.createMarker();
+}
+
+var mapError = function(){
+	console.log('put, put');
 }
 
 // Listen for resize and adjust map
@@ -152,17 +141,33 @@ $(window).resize(function() {
 	resizeMap();
 });
 
+
+
 //Search and List
 var viewModel = function() {
 	var self = this;
 	
 	self.locationList = ko.observableArray([]);
+	self.markerList = ko.observableArray([]);
 	self.query = ko.observable('');
+	self.offlineFallback = ko.observable();
 
-	
-	myNeighborhood.forEach(function(place){
+	manageData.myNeighborhood.forEach(function(place){
 		self.locationList.push(place);
 	});
+	
+	self.markerMove = function(listItem) {
+		var length = self.markerList.length;
+		
+		for (var i = 0; i < length; i++){
+			if(self.markerList[i].title === listItem.name) {
+				self.markerList[i].handleClick();
+			} else {
+				self.markerList[i].infowin.close();
+			}
+		}
+	}
+
 	
 	// Search Function
 	self.search = function() {
@@ -173,7 +178,7 @@ var viewModel = function() {
 		var searchTerm = self.query().trim().toLowerCase();
 
 		// then push only the matching locations back into the array
-		myNeighborhood.forEach(function(x){
+		manageData.myNeighborhood.forEach(function(x){
 			// search by name or by category 
 			if(x.name.toLowerCase().indexOf(searchTerm) >= 0 || x.category.toLowerCase().indexOf(searchTerm) >= 0) {
 				self.locationList.push(x);
@@ -182,4 +187,12 @@ var viewModel = function() {
 	}
 }
 
-ko.applyBindings(new viewModel());
+
+//  viewModel stored in global variable for referencing
+var vm = new viewModel();
+ko.applyBindings(vm);
+
+/*Delay Knockout bindings
+$(window).ready(function () {
+	ko.applyBindings(new viewModel());
+});*/
