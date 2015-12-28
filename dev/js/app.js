e51b8a2b4d68175b5022dbd22388d75590d38eff
Sylvia Schmidt <sylvia.schmidt@jielo.de>
2015-12-28 01:00:39 +0100

    working storage

--- dev/js/app.js
+++ dev/js/app.js
@@ -27,7 +27,7 @@ var manageData = {
 			lng: '10.17927510000004'
 		},
 		{
-			name: 'Lotus-Garten',
+			name: 'Lotos-Garten',
 			description: 'A tranquil south-east asian garden with café in the middle of Europe.',
 			category: 'Restaurant, Food, Outdoor',
 			lat: '49.3677609',
@@ -46,6 +46,64 @@ var manageData = {
 		
 	},
 	
+	updateInfoWindow: function (marker) {
+		console.log(marker.infowin.content);
+		this.getFoursquare(marker);
+	},
+	
+	saveData: function(data){
+		var currentTime = new Date();
+		var currentTime = currentTime.getTime();
+		
+		var oldTime = JSON.parse(localStorage.getItem('time'));
+		
+		var currentStatus = currentTime - oldTime;
+		
+		//Stringify the data.name for localStorage
+		var name = JSON.stringify(data.name);
+		
+		// Store data in localStorage if it hasn't been stored before. 
+		// Or if more than a day has passed, update the data.
+		if (!localStorage.getItem(name)) {
+			localStorage.setItem('time', JSON.stringify(currentTime));
+			localStorage.setItem(name, JSON.stringify(data));
+		} else if (currentStatus > 86400000) {
+			localStorage.removeItem('time');
+			localStorage.removeItem(name);
+			localStorage.setItem('time', JSON.stringify(currentTime));
+			localStorage.setItem(.name, JSON.stringify(data));
+		}
+	},
+	
+	getData: function(){
+		
+	},
+	
+	getFoursquare: function (locData){
+		var fsqQuery = 'https://api.foursquare.com/v2/venues/search' +
+			'?client_id=' + 'OYNXUBHDOXCBUDHXT5D1O14S3K2T1YHRCPA0VF3ZMUUF0DLE' +
+			'&client_secret=' + 'A5TEMADYGHZE0A0H2X3WEF0G0VKPPHYIRDUAXS3CZBDA2WON' +
+			'&v=20130815' +
+			'&intent=match' +
+			'&ll=' + locData.lat + ',' + locData.lng +
+			'&query=' + locData.title;
+		
+		$.getJSON(fsqQuery, function(data) {
+			var results = data.response.venues;
+			
+			manageData.saveData(results[0]);
+		}).fail(function() {
+			console.log('upps');
+		});
+	},
+
+	getStreetView: function (locData) {
+		var url = 'https://maps.googleapis.com/maps/api/streetview?' + 
+		'size=300x300&location=' + locData.lat + ',' + locData.lng + 
+		'&heading=151.78&pitch=-0.76';
+		return url;
+	},
+	
 	createMarker: function() {
 		var mapMarker =[];
 		
@@ -54,9 +112,10 @@ var manageData = {
 			mapMarker.push(marker);
 		});
 		
+		//push the created markes into observableArray
 		vm.markerList(mapMarker);
 	}
-}
+};
 
 
 // Class for map markers
@@ -68,30 +127,42 @@ var Marker = function(map, currentData) {
 	
 	var location = new google.maps.LatLng(dataLat,dataLng);
 	
+	// Create the marker for the current data set
 	marker = new google.maps.Marker({
 			map: map,
 			position: location,
-			title: name
+			title: name,
+			lat: dataLat,
+			lng: dataLng
 	});
 	
+	// Create empty InfoWindow as part of marker object
 	marker.infowin = new google.maps.InfoWindow({
-		content: 'Loading ...'
 	});
 	
+	//marker.infowin.content = ko.observable();
+	
+	//Gets called if marker or list entry is clicked
 	marker.handleClick = function() {
 		marker.infowin.open(map, marker);
 		marker.setAnimation(google.maps.Animation.BOUNCE);
 		setTimeout(function () {
 			marker.setAnimation(null);
 			}, 1400);
-	}
+			
+		marker.infowin.setContent('hey');
+		manageData.updateInfoWindow(marker);
+	};
 	
+	// Event handler for click on marker in map
 	marker.addListener('click', function() {
 		marker.handleClick();
 	});
 	
+	console.log(marker.lat);
+	
 	return marker;
-}
+};
 
 // Google Maps
 
@@ -108,13 +179,13 @@ var initMap = function() {
 		//Calculate and set the height of the map element
 		var mapHeight = ($(window).height() - $('header').outerHeight(true))*0.98;
 		$('#map').height(mapHeight);
-	}
+	};
 	
 	// Adjust map on resize
 	self.resizeMap = function(){
-		scaleMap();
-		map.fitBounds(mapBounds);
-	}
+		self.scaleMap();
+		map.fitBounds(window.mapBounds);
+	};
 	
 	// Center map around locations in myNeighborhood
 	self.findLocation = function() {
@@ -124,38 +195,41 @@ var initMap = function() {
 			map.fitBounds(bounds);
 			map.setCenter(bounds.getCenter());
 		});
-	}
+	};
 	
-	scaleMap();
+	self.scaleMap();
 	window.mapBounds = new google.maps.LatLngBounds();
-	findLocation();
+	self.findLocation();
 	manageData.createMarker();
-}
-
-var mapError = function(){
-	console.log('put, put');
-}
+};
 
 // Listen for resize and adjust map
 $(window).resize(function() {
 	resizeMap();
 });
 
+//
+var mapError = function(){
+	console.log('put, put');
+};
 
 
 //Search and List
 var viewModel = function() {
 	var self = this;
 	
+	// Define observables
 	self.locationList = ko.observableArray([]);
 	self.markerList = ko.observableArray([]);
 	self.query = ko.observable('');
 	self.offlineFallback = ko.observable(false);
-
+	
+	// Populate the locationsList
 	manageData.myNeighborhood.forEach(function(place){
 		self.locationList.push(place);
 	});
 	
+	// Open InfoWindow and bounce markers when a list item is clicked
 	self.markerMove = function(listItem) {
 		var length = self.markerList().length;
 		
@@ -166,13 +240,13 @@ var viewModel = function() {
 				self.markerList()[i].infowin.close();
 			}
 		}
-	}
+	};
 
 	
 	// Search Function
 	self.search = function() {
 		// remove all the locations
-		var removedArray = self.locationList.removeAll();
+		self.locationList.removeAll();
 		
 		//Used for setting marker visiblity
 		var nameObject = {};
@@ -189,16 +263,17 @@ var viewModel = function() {
 			}
 		});
 		
-		//Set marker visibility - see if marker.title is a key in nameObject
+		//Set marker visibility - if marker.title is a key in nameObject
 		self.markerList().forEach(function(marker) {
 			if (marker.title in nameObject){
 				marker.setVisible(true);
 			} else {
 				marker.setVisible(false);
+				marker.infowin.close();
 			}
 		});
-	}
-}
+	};
+};
 
 
 //  viewModel stored in global variable for referencing
